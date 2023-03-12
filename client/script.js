10eb920010462bc6996c695185304e142a7a54ff
Radoslawus <radoslaw.hubert@gmail.com>
2023-03-12 16:43:29 +0100

    first commit

--- client/script.js
+++ client/script.js
@@ -1,42 +1,42 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
+let loadInterval
 
 function loader(element) {
-  element.textContent = '';
+  element.textContent = ''
 
   loadInterval = setInterval(() => {
-    element.textContent += '.';
+    element.textContent += '.'
 
     if (element.textContent === '....') {
-      element.textContent = '';
+      element.textContent = ''
     }
   }, 300)
 }
 
 function typeText(element, text) {
-  let index = 0;
+  let index = 0
 
   let interval = setInterval(() => {
     if(index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
+      element.innerHTML += text.charAt(index)
+      index++
     } else {
-      clearInterval(interval);
+      clearInterval(interval)
     }
   }, 20)
 }
 
 function generateUniqueId() {
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+  const timestamp = Date.now()
+  const randomNumber = Math.random()
+  const hexadecimalString = randomNumber.toString(16)
 
-  return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timestamp}-${hexadecimalString}`
 }
 
 function chatStripe (isAi, value, uniqueId) {
@@ -50,7 +50,7 @@ function chatStripe (isAi, value, uniqueId) {
               alt="${isAi ? 'bot' : 'user'}"
             />
           </div>
-          <div class="message" id=${uniqueId}>${value}</div>
+          <div class="message" id="${uniqueId}">${value}</div>
         </div>
       </div>
     `
@@ -58,24 +58,24 @@ function chatStripe (isAi, value, uniqueId) {
 }
 
 const handleSubmit = async (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
-  const data = new FormData(form);
+  const data = new FormData(form)
 
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-  form.reset();
+  form.reset()
 
   // bot's chatstripe
-  const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+  chatContainer.scrollTop = chatContainer.scrollHeight
 
-  const messageDiv = document.getElementById(uniqueId);
+  const messageDiv = document.getElementById(uniqueId)
 
-  loader(messageDiv);
+  loader(messageDiv)
 
   // fetch data from server -> bot's response
 
@@ -89,26 +89,26 @@ const handleSubmit = async (e) => {
     })
   })
 
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ''
 
   if(response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
+    const data = await response.json()
+    const parsedData = data.bot.trim()
 
-    typeText(messageDiv, parsedData);
+    typeText(messageDiv, parsedData)
   } else {
-    const err = await response.text();
+    const err = await response.text()
 
-    messageDiv.innerHTML = "Something went wrong";
+    messageDiv.innerHTML = "Something went wrong"
 
-    alert(err);
+    alert(err)
   }
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
-    handleSubmit(e);
+    handleSubmit(e)
   }
 })
--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
@@ -44,4 +44,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
